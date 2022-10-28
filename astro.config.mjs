0870a7f9ccd296db97ba8c69edead8a6984806d3
joekroese <joekroese@gmail.com>
2022-10-28 17:58:02 +0200

    add image optimisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,15 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://radicaldata.org",
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
