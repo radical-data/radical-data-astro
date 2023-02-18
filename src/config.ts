6f0b4bfaf1ccb78c791cc8ced0e9f149019279a9
jokroese <jo@jokroese.com>
2023-02-18 00:40:19 -0300

    move global constants to config

---
+++ src/config.ts
@@ -0,0 +1,24 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE = {
+  // Your site's detail?
+  name: "Radical Data",
+  title: "Radical Data - A collective creating an alternative future for data.",
+  description:
+    "A collective claiming data as a tool of resistance and a million liberations.",
+  url: "https://radicaldata.org",
+  // githubUrl: "https://github.com/one-aalam/astro-ink",
+  // listDrafts: true,
+  // image:
+  //   "https://raw.githubusercontent.com/one-aalam/astro-ink/main/public/astro-banner.png",
+  // YT video channel Id (used in media.astro)
+  ytChannelId: "",
+  // Optional, user/author settings (example)
+  // Author: name
+  author: "", // Example: Fred K. Schott
+  // Author: Twitter handler
+  authorTwitter: "radical_data", // Example: FredKSchott
+  // Author: Image external source
+  authorImage: "", // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg
+};
