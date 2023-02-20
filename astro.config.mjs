014fe7c765b0e55c49e74c3150e2bbaeaa180c68
jokroese <jo@jokroese.com>
2023-02-20 12:25:56 -0300

    add astro image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://radicaldata.org',
-  integrations: [sitemap()],
+  site: "https://radicaldata.org",
+  integrations: [
+    sitemap(),
+    [
+      image({
+        serviceEntryPoint: "@astrojs/image/sharp",
+      }),
+    ],
+  ],
 });
