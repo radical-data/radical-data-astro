89ceaa12c703b2a38e4a2fa695b958b4264f68ae
Jo Kroese <jo@jokroese.com>
2023-11-21 19:03:23 +0100

    error for empty posts

--- src/lib/mostRecentPosts.js
+++ src/lib/mostRecentPosts.js
@@ -1,7 +1,13 @@
 export function mostRecentPosts(allPosts, field, sliceNumber) {
+  if (!Array.isArray(allPosts) || allPosts.length === 0) {
+    console.error('Invalid or empty posts array');
+    return [];
+  }
+
   const sortedPosts = allPosts.sort(
     (a, b) => new Date(a.fields[field]) - new Date(b.fields[field])
   );
+
   const sortedSlicedPosts = sortedPosts.slice(sliceNumber);
   console.log(sortedSlicedPosts);
   return sortedSlicedPosts;
