68a4d13431dc4825c7f08709b00e41968f39b1cf
Jo Kroese <jo@jokroese.com>
2025-11-18 18:54:44 +0100

    feat: upgrade to Astro v5 and remove legacy image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://radicaldata.org",
-  integrations: [
-    sitemap(),
-    [
-      image({
-        serviceEntryPoint: "@astrojs/image/sharp",
-      }),
-    ],
-  ],
+  integrations: [sitemap()],
 });
