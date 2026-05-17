ecbcd9ce02df562a308b3c78e964a260bf285222
Jo Kroese <jo@jokroese.com>
2026-05-17 12:39:41 +0200

    feat: migrate to self-hosted plausible

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="astro/client" />
+
+type PlausibleOptions = {
+  props?: Record<string, string | number | boolean | null>;
+
+  revenue?: {
+    currency: string;
+
+    amount: number;
+  };
+
+  callback?: () => void;
+};
+
+interface PlausibleFn {
+  (eventName: string, options?: PlausibleOptions): void;
+
+  q?: IArguments[];
+}
+
+interface Window {
+  plausible: PlausibleFn;
+}
